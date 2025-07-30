fae3b7d2193f077609a0e2d61fec48fbe18eb9dc
daniel-yesa <daniel@yesa.ca>
2025-07-30 10:18:37 -0400

    Update wsgi.py

--- autoMate/wsgi.py
+++ autoMate/wsgi.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from main import create_app
 
@@ -9,3 +10,7 @@ logger.info("🔄 Starting via WSGI...")
 app = create_app()  # ✅ Assign the result to `app`
 
 logger.info("✅ WSGI app created")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # Railway will set this
+    app.run(host="0.0.0.0", port=port)
