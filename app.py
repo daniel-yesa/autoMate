41dd9b2e11201591b0cdd0f06b87397ff188bd1d
daniel-yesa <daniel@yesa.ca>
2025-07-30 17:43:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template
-from findr import findr_bp  # Now this will work
+import os
 
 app = Flask(__name__)
-
-# Register blueprint
-app.register_blueprint(findr_bp)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
